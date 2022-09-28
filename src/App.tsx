04b6d0a1ad9c82556e7857590cc0b9752ccddce6
gab5987 <biell.gb4@gmail.com>
2022-09-28 12:06:18 -0300

    Fix: Change to scss and implement sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProSidebar className='App-sidebar'>
+        <Menu iconShape="square">
+          <MenuItem >Dashboard</MenuItem>
+          <SubMenu title="Components" >
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </Menu>
+      </ProSidebar>
+
+      <div className="App-header">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
