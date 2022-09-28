0530fe10156ad456052409df78830cb6d66f0dad
gab5987 <biell.gb4@gmail.com>
2022-09-28 17:36:40 -0300

    Feat: Added template for login screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,40 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Sidebar from './components/Sidebar';
 import Homepage from './components/pages/Home';
-class App extends React.Component<{}, {actualPage: string} > {
+import UserLogin from './components/pages/UserLogin';
+
+const ptBR = require("./data/res_primary_language.json");
+const enUS = require("./data/res_secondary_language.json");
+const data = require("./data/shared_data.json");
+
+class App extends Component {
+  constructor(props: any){
+    super(props);
+    this.state = {
+      user: undefined,
+      language: "ptBR",
+      isLogged: false,
+    }
+  }
+  render(): React.ReactNode {
+      return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" element={<Main />} />
+            <Route path="/user/login" element={<UserLogin/>} />
+          </Routes>
+        </BrowserRouter>
+      )
+  }
+}
+class Main extends React.Component<{}, {actualPage: string, language: object, user: any, isLogged: boolean} > {
   constructor(props: any) {
     super(props);
     this.state = {
-      actualPage: window.location.pathname
+      actualPage: window.location.pathname,
+      language: ptBR,
+      user: undefined,
+      isLogged: false,
     }
   }
 
@@ -17,12 +46,9 @@ class App extends React.Component<{}, {actualPage: string} > {
       <div className="App">
         <Sidebar page={this.state.actualPage}/>
         <div className="App-header">
-          { this.state.actualPage === '/home' && <Homepage/>}
+          { this.state.actualPage === '/home' && <Homepage resumeLanguage={this.state.language}/>}
           { this.state.actualPage === '/about' ? <About/> : <Unreachable/>}
         </div>
-        <BrowserRouter>
-          <Routes/>  
-        </BrowserRouter>
       </div>
     );
   }
@@ -33,6 +59,11 @@ const About = () => {
 }
 
 const Unreachable = () => {
-  return <h1>404 - not found !</h1>
+  return(
+    <div>
+      <h1>404 - not found !</h1>
+      <a href="/home" style={{textDecoration: "none"}}> go back home </a>
+    </div>
+  );
 }
 export default App;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,6 @@
 import logo from '../../logo.svg';
 
-const Homepage = () => {
+const Homepage = (props: any) => {
     return (
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
